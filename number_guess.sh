0b8266766cf9150d5bb16e24e5b59643291586d8
chemikas <chemikas@yahoo.com>
2022-10-11 09:35:53 +0000

    feat: added random genereator

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
 echo -e "\nEnter your username:"
+read NAME
+
+echo -e "Welcome back, $NAME! You have played <games_played> games, and your best game took <best_game> guesses."
+
+echo $(( $RANDOM % 1000 + 1 ))
